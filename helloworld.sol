9233e9a5b664dec9f9f7441b5b2a771e1a462749
Mobin Ranjbar <mobinranjbar@hotmail.com>
2018-02-03 10:14:30 +0330

    Smart Contract added.

---
+++ helloworld.sol
@@ -0,0 +1,16 @@
+/* Ethereum Simple Smart Contract
+ *
+ * Written in Solidity
+ *
+ * By Farafekr Technology (@farafekr)
+ * Author : @mobinranjbar
+ *
+ */
+pragma solidity ^0.4.0;
+contract HelloWorld {
+    uint balance;
+    
+    function HelloWorld() public{
+        balance=100;
+    }
+}
